9a615c6df45956dc7f0ae5033e9e5caeea4f68e4
Miah Harper <harpermiah@yahoo.com>
2022-06-13 15:40:55 -0400

    finished assignment

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+
+		private double _balance = 0;
+
+		public void Deposit(double amount)
+		{
+
+			_balance += amount;
+
+		}
+		public void Withdraw(double amount)
+		{
+			_balance -= amount;
+		}
+
+		public double GetBalance()
+        {
+			return _balance;
+        }
+
+
+	}
+}
+		
+		
+
+    
+			
+	
+
+
+
+
+
+
+
+			
+
+
+
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount();
+
+            bankAccount.Deposit(20.29);
+            bankAccount.Withdraw(10.11);
+            var amount = bankAccount.GetBalance();
+
+            Console.WriteLine($"Your balance is {amount}");
+        }
+    }
+}
+
